b8189e47a147daa064bb723603bc7caed641280b
thatsymer <84711212+thatsymer@users.noreply.github.com>
2021-08-21 21:44:46 +0800

    replaced list with card

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const addBtn = document.getElementById("add-btn");
-const ulEl = document.getElementById("ul-el");
+const card = document.getElementById("card");
 const inputField = document.getElementById("input-field");
 const clearBtn= document.getElementById("clear-btn");
 const todoList= document.getElementById("todo-list");
@@ -17,28 +17,36 @@ function renderList(){
         let getLocalStorageData = localStorage.getItem("New Todo"); //getting localstorage
         if(getLocalStorageData == null){ //if localstorage has no data
             list = []; //create a blank array
-        }else{
+        }
+        else{
             list = JSON.parse(getLocalStorageData);  //transforming json string into a js object
         }
         list.push(inputField.value)
         localStorage.setItem("New Todo", JSON.stringify(list));
         let listItems= ""  
+        
+        
         for (let i=0; i<list.length; i++){
                 listItems += `
-                    <li id="chk" class='nes-container is-dark'>
-                        <input id="toggle" type="checkbox">
-                        <label>${list[i]}</label>
-                    </li>
+                <div class="card__item">
+                    <div class="nes-container is-dark">
+                        <span class="position-absolute top-0 end-0 fs-4 text-danger" id="close-card-${i}"> <i class='bx bx-x'></i></span>
+                        <h6>${list[i]}</h6>
+                    </div>
+                </div>
                 `
                 
             }
             
             localStorage.setItem("New Todo", JSON.stringify(list)); //transforming js object into a json string
-            ulEl.innerHTML = listItems
+            card.innerHTML = listItems
             inputField.value = ''
         }
+    
     function clearList(){
         list = []
-        ulEl.innerHTML= "<li> </li>"
+        card.innerHTML= '<div class="cards" id="card"></div>'
+
         localStorage.setItem("New Todo", JSON.stringify(list)); //set the item in localstorage
     }
+
